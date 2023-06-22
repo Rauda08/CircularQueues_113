96393419e3858d3b424ac701c787bb9eccc1651a
rauda08 <raudajnnh08@gmail.com>
2023-06-22 15:45:30 +0700

    menampilkan display 1-4

--- CircularQueues_113/CircularQueues_113.cpp
+++ CircularQueues_113/CircularQueues_113.cpp
@@ -106,7 +106,10 @@ int main() {
 	while (true) {
 		try {
 			cout << "Menu" << endl;
-			
+			cout << "1. Implement insert operation" << endl;
+			cout << "2. Implement insert operation" << endl;
+			cout << "3. Display values" << endl;
+			cout << "4. Exit" << endl;
 		}
 	}
 
